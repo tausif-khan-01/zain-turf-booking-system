e418b88f822a1e203fe4ad69ec3685c74ef70aef
Tausif Khan <tausifahmedkhan01@gmail.com>
2025-04-19 15:36:30 +0530

    Update header component to improve z-index for better visibility: adjusted z-index in header styling to ensure it remains on top of other elements.

--- admin/src/components/layout/header.jsx
+++ admin/src/components/layout/header.jsx
@@ -23,7 +23,7 @@ function AdminHeader() {
     return "Dashboard";
   };
   return (
-    <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4 justify-between">
+    <header className="sticky z-10 top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4 justify-between">
       <div className="flex items-center">
         <SidebarTrigger className="mr-2" />
         <h1 className="text-xl font-semibold hidden md:block">
